69072ad035ad9c9bee4a835484a06419c993fec6
zhanghebin <hebin.zhang@hand-china.com>
2019-04-28 18:05:24 +0800

    Merge branch 'master' of D:\projects\leaf-zgczl with conflicts.

--- core/src/main/webapp/WEB-INF/server-script/layoutconfig/bank_interface_logs_g_result_queryfields.js
+++ core/src/main/webapp/WEB-INF/server-script/layoutconfig/bank_interface_logs_g_result_queryfields.js
@@ -10,6 +10,10 @@ var override_queryfields = [
 	queryexpression : "substr(t1.request_time,0,10) = ${@request_time}"
 },
     {
+        name : 'operation_time',
+        queryexpression : "substr(t1.operation_time,0,10) = ${@operation_time}"
+    },
+    {
         name : 'recipname',
         queryexpression : "t1.recipname like '%'||${@recipname}||'%'"
     },
